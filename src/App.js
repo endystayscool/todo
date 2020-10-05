910c4ad44d3241b1910a6c1ab028c3b057dca6d2
endyjtwn <endyjtwn@gmail.com>
2020-10-06 01:40:22 +0200

    plus button

--- src/App.js
+++ src/App.js
@@ -8,9 +8,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>todo</h1>
+        <a href="#" class="btn btn-outline-info rounded-circle add">+</a>
       </header>
       <div class="container">
         <div class="row">
+          <div class="card">
+            {/* <img src="" class="card-img-top" alt="" /> */}
+            <div class="card-body separate">
+              <h6 class="card-title">Card title</h6>
+              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+              <a href="#" class="btn btn-outline-danger delete">Delete</a>
+            </div>
+          </div>
+          <div class="card">
+            {/* <img src="" class="card-img-top" alt="" /> */}
+            <div class="card-body separate">
+              <h6 class="card-title">Card title</h6>
+              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+              <a href="#" class="btn btn-outline-danger delete">Delete</a>
+            </div>
+          </div>
           <div class="card">
             {/* <img src="" class="card-img-top" alt="" /> */}
             <div class="card-body separate">
