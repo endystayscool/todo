d3e4acd7fa0b0af6951549195889496f499d2bae
endyjtwn <endyjtwn@gmail.com>
2020-10-06 01:05:02 +0200

    main ui

--- src/App.js
+++ src/App.js
@@ -7,18 +7,28 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>todo</h1>
       </header>
+      <div class="container">
+        <div class="row">
+          <div class="card">
+            {/* <img src="" class="card-img-top" alt="" /> */}
+            <div class="card-body separate">
+              <h6 class="card-title">Card title</h6>
+              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+              <a href="#" class="btn btn-outline-danger delete">Delete</a>
+            </div>
+          </div>
+          <div class="card">
+            {/* <img src="" class="card-img-top" alt="" /> */}
+            <div class="card-body separate">
+              <h6 class="card-title">Card title</h6>
+              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+              <a href="#" class="btn btn-outline-danger delete">Delete</a>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
