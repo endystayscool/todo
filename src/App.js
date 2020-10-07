d181f46440a5e215ca6bd8a9e2989b17cda99094
endyjtwn <endyjtwn@gmail.com>
2020-10-08 01:10:51 +0200

    style change

--- src/App.js
+++ src/App.js
@@ -2,50 +2,140 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <h1>todo</h1>
+//         <a href="#" class="btn btn-outline-info rounded-circle add">+</a>
+//       </header>
+//       <div class="container">
+//         <div class="row">
+//           <div class="card">
+//             {/* <img src="" class="card-img-top" alt="" /> */}
+//             <div class="card-body separate">
+//               <h6 class="card-title">Card title</h6>
+//               <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+//               <a href="#" class="btn btn-outline-danger delete">Delete</a>
+//             </div>
+//           </div>
+//           <div class="card">
+//             {/* <img src="" class="card-img-top" alt="" /> */}
+//             <div class="card-body separate">
+//               <h6 class="card-title">Card title</h6>
+//               <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+//               <a href="#" class="btn btn-outline-danger delete">Delete</a>
+//             </div>
+//           </div>
+//           <div class="card">
+//             {/* <img src="" class="card-img-top" alt="" /> */}
+//             <div class="card-body separate">
+//               <h6 class="card-title">Card title</h6>
+//               <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+//               <a href="#" class="btn btn-outline-danger delete">Delete</a>
+//             </div>
+//           </div>
+//           <div class="card">
+//             {/* <img src="" class="card-img-top" alt="" /> */}
+//             <div class="card-body separate">
+//               <h6 class="card-title">Card title</h6>
+//               <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+//               <a href="#" class="btn btn-outline-danger delete">Delete</a>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }function App() {
+
 function App() {
   return (
-    <div className="App">
+    <div className="wrapper">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>todo</h1>
-        <a href="#" class="btn btn-outline-info rounded-circle add">+</a>
+        <h1>header</h1>
       </header>
-      <div class="container">
-        <div class="row">
-          <div class="card">
-            {/* <img src="" class="card-img-top" alt="" /> */}
-            <div class="card-body separate">
-              <h6 class="card-title">Card title</h6>
-              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
-              <a href="#" class="btn btn-outline-danger delete">Delete</a>
-            </div>
+      <div className="App-navbar">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="navname">navbar</h1>
+        <a href="#" class="btn btn-outline-info rounded-circle add">+</a>
+      </div>
+      <div className="content-wrapper">
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
           </div>
-          <div class="card">
-            {/* <img src="" class="card-img-top" alt="" /> */}
-            <div class="card-body separate">
-              <h6 class="card-title">Card title</h6>
-              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
-              <a href="#" class="btn btn-outline-danger delete">Delete</a>
-            </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
           </div>
-          <div class="card">
-            {/* <img src="" class="card-img-top" alt="" /> */}
-            <div class="card-body separate">
-              <h6 class="card-title">Card title</h6>
-              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
-              <a href="#" class="btn btn-outline-danger delete">Delete</a>
-            </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
           </div>
-          <div class="card">
-            {/* <img src="" class="card-img-top" alt="" /> */}
-            <div class="card-body separate">
-              <h6 class="card-title">Card title</h6>
-              <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
-              <a href="#" class="btn btn-outline-danger delete">Delete</a>
-            </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
+          </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
+          </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
+          </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
+          </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
+          </div>
+        </div>
+        <div class="card">
+          {/* <img src="" class="card-img-top" alt="" /> */}
+          <div class="card-body separate">
+            <h6 class="card-title">Card title</h6>
+            <h3 class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</h3>
+            <a href="#" class="btn btn-outline-danger delete">Delete</a>
           </div>
         </div>
       </div>
+
     </div>
   );
 }
